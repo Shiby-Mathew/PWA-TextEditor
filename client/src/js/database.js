6047168548f931f80431622fc182cd14cac19304
Shiby-Mathew <shiby.ajay@gmail.com>
2023-06-25 15:43:53 +1000

    working on error issues

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,11 +25,11 @@ export const putDb = async (content) => {
   // Open up the desired object store.
   const store = tx.objectStore("jate");
   // Use the .put() method to update data in the database
-  const request = store.put({ id: 1, jate: content });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
-  console.log("jate data saved to the database", result);
+  console.log("jate data saved to the database", result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -46,13 +46,14 @@ export const getDb = async () => {
   // Open up the desired object store.
   const store = tx.objectStore("jate");
 
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  // Use the .get() method to get the data in the database.
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log("Get data to the database", result);
-  return result;
+  return result ? result.value : null;
+  // return result;
 };
 
 initdb();
